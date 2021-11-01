00699c09a2aa45101211fd1f5463f64d2d7c1d44
Bobby Kimani <bobbykimani@Bobbys-MacBook-Pro.local>
2021-11-01 12:42:05 +0300

    Calculates the factorial

---
+++ factorial.py
@@ -0,0 +1,22 @@
+#this function computes the factorial
+
+#of a number entered by the user
+
+
+def factor():
+
+    n = int(input("Please enter a whole number:"))
+
+    fact = 1
+
+  
+
+    for factor in range (1, n+1):
+
+        fact = fact * factor
+
+       
+
+    print("The factorial of ",n, "is",fact)
+
+factor()
